4643dd2535bcfae70b34cdc35f0fa260a3dad62b
Audrey <74775317+OdriWan@users.noreply.github.com>
2021-11-17 17:03:22 +0100

    Changement du nom

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MonApplication extends StatelessWidget {
+  const MonApplication({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
