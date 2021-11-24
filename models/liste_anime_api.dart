51595d9fa4a4bb5fbc62d15197f432eaff67f662
Audrey <74775317+OdriWan@users.noreply.github.com>
2021-11-24 17:02:59 +0100

    Json liste anime

---
+++ models/liste_anime_api.dart
@@ -0,0 +1,22 @@
+import 'package:json_annotation/json_annotation.dart';
+
+
+/// flutter packages pub run build_runner build --delete-conflicting-outputs
+part 'liste_anime_api.g.dart';
+
+@JsonSerializable()
+class Listeanimeapi {
+  final int rank;
+  final String title;
+  final String image_url;
+  final String type;
+  final int episodes;
+  final DateTime start_date;
+  final DateTime end_date;
+
+  Listeanimeapi(this.rank, this.title, this.image_url, this.type, this.episodes, this.start_date, this.end_date);
+
+  factory Listeanimeapi.fromJson(Map<String, dynamic> json) => _$ListeAnimeApiFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ListeAnimeApiToJson(this);
+}
