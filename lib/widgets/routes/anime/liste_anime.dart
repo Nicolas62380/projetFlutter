4aa86d59882fc1605f387adb6da52c3f1cda5ae5
nicolas <berqueznicolas@gmail.com>
2021-11-24 15:36:05 +0100

    Debut ListeAnime

--- lib/widgets/routes/anime/liste_anime.dart
+++ lib/widgets/routes/anime/liste_anime.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
-class Liste extends StatelessWidget {
-  const Liste({Key? key}) : super(key: key);
+class ListeAnime extends StatefulWidget {
+  const ListeAnime({Key? key}) : super(key: key);
+
+  @override
+  State<ListeAnime> createState() => _ListeAnime();
+}
+
+class _ListeAnime extends State<ListeAnime> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Liste Animes"),
+      ),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Text("Item N°$index");
+        },
+      ),
+    );
   }
 }
