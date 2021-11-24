4146143902425c93b7c873de9f4cdad3da6a9472
pochmax <65902021+pochmax@users.noreply.github.com>
2021-11-24 15:06:50 +0100

    routes, home, detail, liste

--- lib/widgets/routes/anime/detail_anime.dart
+++ lib/widgets/routes/anime/detail_anime.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class Detail extends StatelessWidget {
+  const Detail({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
