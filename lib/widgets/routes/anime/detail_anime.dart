1a559798d840ce0fdff967697ce7a5096ac41f4d
Max Pochet <max.pochet@orange.fr>
2021-11-24 17:32:53 +0100

    commit fin de cours

--- lib/widgets/routes/anime/detail_anime.dart
+++ lib/widgets/routes/anime/detail_anime.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class Detail extends StatelessWidget {
@@ -7,30 +5,18 @@ class Detail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-
-class HomeImage extends StatelessWidget {
-  const HomeImage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Row(
-          children: [
-            Container(
-              color: Colors.red,
-              width: 200,
-              height: 200,
-            ),
-            const Text("Ceci est un test"),
-          ],
-        )),
+          child: Row(
+        children: [
+          Container(
+            color: Colors.red,
+            width: 200,
+            height: 200,
+          ),
+          const Text("Ceci est un test"),
+        ],
+      )),
     );
   }
 }
