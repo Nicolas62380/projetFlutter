ef4a7f41cffd85d7b2e63642b771f32d75f1a386
pochmax <max.pochet@orange.fr>
2021-11-24 16:46:34 +0100

    detail

--- lib/widgets/routes/anime/detail_anime.dart
+++ lib/widgets/routes/anime/detail_anime.dart
@@ -7,30 +7,19 @@ class Detail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-
-class HomeImage extends StatelessWidget {
-  const HomeImage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Row(
-          children: [
-            Container(
-              color: Colors.red,
-              width: 200,
-              height: 200,
-            ),
-            const Text("Ceci est un test"),
-          ],
-        )),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            color: Colors.red,
+            width: 200,
+            height: 200,
+          ),
+          const Text("Ceci est un test"),
+        ],
+      )),
     );
   }
 }
