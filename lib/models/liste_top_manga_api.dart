c31167723131d0931568759d4c604b76efeee886
nicolas <berqueznicolas@gmail.com>
2021-12-01 17:42:30 +0100

    liste_top_manga_api DART files

---
+++ lib/models/liste_top_manga_api.dart
@@ -0,0 +1,16 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:projetflutter/models/anime_api.dart';
+
+
+/// flutter packages pub run build_runner build --delete-conflicting-outputs
+part 'liste_top_manga_api.g.dart';
+
+@JsonSerializable()
+class ListeTopMangaApi {
+  final List<AnimeApi> top;
+  ListeTopMangaApi(this.top);
+
+  factory ListeTopMangaApi.fromJson(Map<String, dynamic> json) => _$ListeTopMangaApiFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ListeTopMangaApiToJson(this);
+}
---
+++ lib/providers/top_manga_provider.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:projetflutter/models/manga_api.dart';
+import 'package:projetflutter/models/liste_top_api.dart';
+import 'package:projetflutter/providers/dio.dart';
+
+final topMangaProvider = FutureProvider<ListeTopApi?>((ref) async {
+  final Dio dio = ref.read(dioProvider);
+
+  try{
+    final Response response = await dio.get('/top/manga/1');
+    return ListeTopApi.fromJson(response.data);
+  }catch(e){
+    return Future.error(e);
+  }
+});
+
+//async permet de ne pas attendre que la fonction aie terminé son travail (tourne en fond)
