374c8369a6387acf38698a1fc3d8eaad303f0af9
Max Pochet <max.pochet@orange.fr>
2021-12-01 14:08:08 +0100

    dio

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:projetflutter/widgets/routes/home/home.dart';
 import 'package:projetflutter/widgets/routes/anime/detail_anime.dart';
 import 'package:projetflutter/widgets/routes/anime/liste_anime.dart';
@@ -14,13 +15,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: ' Demo',
-      home: const Home(),
-      routes: {
+        title: ' Demo',
+        home: ProviderScope(
+          child: /*const Home()*/ TestDio(),
+          /*routes: {
         '/home': (context) => const Home(),
         '/detail_anime': (context) => const Detail(),
         '/liste_anime': (context) => const ListeAnime(),
-      },
-    );
+      },*/
+        ));
+  }
+}
+
+class TestDio extends ConsumerWidget {
+  const TestDio({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final Dio dio = ref.read(dioProvider);
+
+    dio.get('end-of-url?u=1881');
+    return Container();
   }
 }
---
+++ lib/providers/dio.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final Dio dio = Dio(BaseOptions(
+    baseUrl: "https://api.jikan.moe/v3/top/anime/1/airing",
+    connectTimeout: 1000,
+    receiveTimeout: 5000,
+    sendTimeout: 5000));
+
+final dioProvider = Provider<Dio>((ref) {
+  return dio;
+});
