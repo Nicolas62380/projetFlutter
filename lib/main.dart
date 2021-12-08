6ffc62b5f88711fc35253b26dcf313dd1ebad8fd
jérémy_laurent <jeremy.laurentj@gmail.com>
2021-12-08 15:47:20 +0100

    import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:projetflutter/widgets/manga.dart';
 
 
 import 'package:projetflutter/widgets/navbar/navbar.dart';
